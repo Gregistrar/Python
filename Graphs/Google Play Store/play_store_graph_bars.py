34d7bb0b5d44ab326aba070272f69a46f36d8e44
Gregistrar <ghodgso1@gmail.com>
2018-10-08 16:37:59 -0400

    update

---
+++ Graphs/Google Play Store/play_store_graph_bars.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+
+google_data = pd.read_csv("\\Users\ghodg\Desktop\Projects\Python\Graphs\Google Play Store\googleplaystore.csv",
+                   encoding='utf-8')
+google_data.head()
+google_data.info()
+
+
+
+
+
