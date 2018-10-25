c038d20ea0142a490b4edaa50d816ba1224c7cf2
Gregistrar <ghodgso1@gmail.com>
2018-10-24 23:00:30 -0400

    update 10/24/18

--- Iris Dataset Project/iris_data_exploration.py
+++ Iris Dataset Project/iris_data_exploration.py
@@ -1,10 +1,25 @@
+# https://archive.ics.uci.edu/ml/datasets/Iris
+# https://www.slideshare.net/thoi_gian/iris-data-analysis-with-r
+# https://www.marsja.se/pandas-python-descriptive-statistics/
 import pandas as pd
+import numpy as np
+from scipy.stats import trim_mean, kurtosis
+from scipy.stats.mstats import mode, gmean, hmean
+
+iris = pd.read_csv('C:\\Users\\ghodg\\Desktop\\Projects\\Python\\Iris Dataset Project\\iris.data.txt',
+                   names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+iris.info()
+iris.head()
+
+# Pandas will output summary statistics by using this method
+iris.describe()
+
+grouped_data = iris.groupby(['class'])
+grouped_data['sepal_length', 'sepal_width', 'petal_length', 'petal_width'].describe()
+
+# Pandas Mean, both are doing the same thing
+grouped_data['sepal_length', 'sepal_width', 'petal_length', 'petal_width'].mean().reset_index()
+grouped_data['sepal_length', 'sepal_width', 'petal_length', 'petal_width'].aggregate(np.mean).reset_index()
 
 
-iris =
-
-
-
-
-
-
+# Geometric & Harmonic mean
